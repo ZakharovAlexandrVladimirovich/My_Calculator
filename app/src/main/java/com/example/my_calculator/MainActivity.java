4412a0f869ae397bf575218d9ca1bc96b2f31825
Zakharov Aleksandr <Alex362838969@gmail.com>
2021-07-06 22:11:43 +0300

    style for button added app background added

--- app/src/main/java/com/example/my_calculator/MainActivity.java
+++ app/src/main/java/com/example/my_calculator/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.my_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button  button1 = findViewById(R.id.button4);
+        EditText editText = findViewById(R.id.editTextTextMultiLine);
+
+        button1.setOnClickListener(v -> editText.setText(String.format("%s", "12345")));
+
     }
 }
