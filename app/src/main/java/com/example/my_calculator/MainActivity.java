a490ee76d4e35b0b08647a890a02be9d039dd0f0
Zakharov Aleksandr <Alex362838969@gmail.com>
2021-07-12 02:13:00 +0300

    redactNew

--- app/src/main/java/com/example/my_calculator/MainActivity.java
+++ app/src/main/java/com/example/my_calculator/MainActivity.java
@@ -3,38 +3,67 @@ package com.example.my_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private final int[] buttons = {R.id.button0, R.id.button1, R.id.button2, R.id.button3,
-            R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9};
+            R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9,
+            R.id.buttonDivision, R.id.buttonMultiplication, R.id.buttonPoint, R.id.buttonSubtraction,
+            R.id.buttonSum,R.id.buttonEqual,R.id.buttonANegativeNumber};
+    private final String[] buttonsValue = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/",
+            "*", ".", "-", "+","=","-"};
     EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        buttonClickInitialaizer();
+
+        buttonClickInitialaizer2();
         editText = findViewById(R.id.editTextTextMultiLine);
+
+//        Button button = findViewById(R.id.buttonMultiplication);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                editText.setText("*");
+//            }
+//        });
+
+
     }
-    private void buttonClickInitialaizer() {
+
+    private void buttonClickInitialaizer2() {
         int i = 0;
         for (int button : buttons) {
-            setButtonClickListeners(button, i++);
+            setButtonClickListeners2(button, buttonsValue[i++]);
         }
     }
-    private void setButtonClickListeners(int button, int number) {
-        findViewById(button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainActivity.this.setText(number);
-            }
-        });
+
+    private void setButtonClickListeners2(int button, String number) {
+        findViewById(button).setOnClickListener(v -> setText2(number));
     }
-    public void setText(int string) {
+
+    public void setText2(String string) {
         String buffer = editText.getText().toString();
         editText.setText(String.format("%s", buffer + string));
+//        editText.append(String.valueOf(string));
     }
+
+
+//    private void buttonClickInitialaizer() {
+//        int i = 0;
+//        for (int button : buttons) {
+//            setButtonClickListeners(button, i++);
+//        }
+//    }
+//    private void setButtonClickListeners(int button, int number) {
+//        findViewById(button).setOnClickListener(v -> setText(number));
+//    }
+//    public void setText(int string) {
+//        String buffer = editText.getText().toString();
+//        editText.setText(String.format("%s", buffer + string));
+////        editText.append(String.valueOf(string));
+//    }
 
 }
