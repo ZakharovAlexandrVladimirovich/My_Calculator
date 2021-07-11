85751e93422812071ea4cb78a5d1a62448dd2ef6
Zakharov Aleksandr <Alex362838969@gmail.com>
2021-07-11 17:46:45 +0300

    redact2

--- app/src/main/java/com/example/my_calculator/MainActivity.java
+++ app/src/main/java/com/example/my_calculator/MainActivity.java
@@ -11,31 +11,19 @@ public class MainActivity extends AppCompatActivity {
     private final int[] buttons = {R.id.button0, R.id.button1, R.id.button2, R.id.button3,
             R.id.button4, R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9};
     EditText editText;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonClickInitialaizer();
-//        Button button1 = findViewById(R.id.button1);
         editText = findViewById(R.id.editTextTextMultiLine);
-//        button1.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                setText("1");
-//            }
-//        });
-//        Button button2 = findViewById(R.id.button2);
-//        button2.setOnClickListener(v -> setText("2"));
     }
-
     private void buttonClickInitialaizer() {
         int i = 0;
         for (int button : buttons) {
             setButtonClickListeners(button, i++);
         }
     }
-
     private void setButtonClickListeners(int button, int number) {
         findViewById(button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -44,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
     public void setText(int string) {
         String buffer = editText.getText().toString();
         editText.setText(String.format("%s", buffer + string));
